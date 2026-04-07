65a194ddf4d3e9ad6852b6ca9fd6a9664a64ce14
sruti2253-oss <sruti2253@gmail.com>
2026-04-07 14:47:44 +0530

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,1 +1,11 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
 
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        result = emotion_detector("I am very happy today!")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+if __name__ == "__main__":
+    unittest.main()
